26770cacae44c29b32c24a84c417a2c9f1355e79
dimsen <dimas.10119285@mahasiswa.unikom.ac.id>
2021-10-07 18:29:56 +0700

    first commit

---
+++ app/src/main/java/com/dimsen/kedara/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dimsen.kedara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
